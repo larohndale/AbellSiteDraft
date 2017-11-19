45678cddb916d3bfead52a88c21504089d82cda1
Domqinque Cox <larohndale@gmail.com>
2017-11-19 12:44:48 -0500

    Added a custom.js file to the index.html page  for a future link on the consultation form to pull the data to a back end server. Plugins will be executed in the futre.

---
+++ custom.js
@@ -0,0 +1,17 @@
+// custom javascript until Python course to actually work with back end data
+
+
+var repairType = prompt("What type of repair do you need?");
+
+var rapairDate = prompt("What date do you need your repair completed?");
+
+// alert("We have received your request and will be back with you shortly!")
+
+
+
+
+if (confirm("Do you understand this data will not be submitted until future updates") == true) {
+    txt = "You pressed OK!";
+} else {
+    txt = "You pressed Cancel!";
+}
